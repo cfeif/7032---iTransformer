27bd981c15b2cdb9182c1a2dbc3fcd14324b52fb
cfeif <95038304+cfeif@users.noreply.github.com>
2024-04-21 12:42:10 +0800

    Latest

--- scripts/multivariate_forecasting/Equity/iTransformer.sh
+++ scripts/multivariate_forecasting/Equity/iTransformer.sh
@@ -2,23 +2,61 @@ export CUDA_VISIBLE_DEVICES=3
 
 model_name=iTransformer
 
-python -u run.py \
+# python -u run.py \
+#   --is_training 1 \
+#   --root_path ./dataset/equity/ \
+#   --data_path daily_subset.csv \
+#   --model_id Equity_96_1 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --pred_len 1 \
+#   --e_layers 3 \
+#   --enc_in 64 \
+#   --dec_in 64 \
+#   --c_out 64 \
+#   --des 'Exp' \
+#   --d_model 512 \
+#   --d_ff 512 \
+#   --batch_size 16 \
+#   --learning_rate 0.0005 \
+#   --itr 1
+
+# python -u run.py \
+#   --is_training 1 \
+#   --root_path ./dataset/equity/ \
+#   --data_path stock_402_nonan_industry30_stocks.csv \
+#   --model_id stock_402_nonan_industry30_48_12 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 48 \
+#   --pred_len 12 \
+#   --e_layers 4 \
+#   --enc_in 182 \
+#   --dec_in 182 \
+#   --c_out 182 \
+#   --des 'Exp' \
+#   --d_model 512 \
+#   --d_ff 512 \
+#   --itr 1
+
+  python -u run.py \
   --is_training 1 \
   --root_path ./dataset/equity/ \
-  --data_path daily_subset.csv \
-  --model_id Equity_96_1 \
+  --data_path stock_596.csv \
+  --model_id stock_596_21dayreturn_48_1 \
   --model $model_name \
   --data custom \
   --features M \
-  --seq_len 96 \
+  --seq_len 48 \
   --pred_len 1 \
-  --e_layers 3 \
-  --enc_in 64 \
-  --dec_in 64 \
-  --c_out 64 \
+  --e_layers 4 \
+  --enc_in 14 \
+  --dec_in 14 \
+  --c_out 14 \
   --des 'Exp' \
   --d_model 512 \
   --d_ff 512 \
-  --batch_size 16 \
-  --learning_rate 0.0005 \
   --itr 1
